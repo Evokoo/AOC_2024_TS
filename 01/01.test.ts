d54a03d739f5e7e45f5f755a4d7528efd10a0714
Evokoo <evanjohncooper@gmail.com>
2024-12-01 16:29:47 +1100

    Day 01 complete

--- 01/01.test.ts
+++ 01/01.test.ts
@@ -8,21 +8,21 @@ const currentDay = path.basename(Deno.cwd());
 describe(`AOC 2024 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
-		});
-
-		it.ignore("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
-		});
-	});
-
-	describe.ignore("Part B", () => {
-		it("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay)).toBe(11);
 		});
 
 		it("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+			expect(solveA("input", currentDay)).toBe(3246517);
+		});
+	});
+
+	describe("Part B", () => {
+		it("Example", () => {
+			expect(solveB("example_b", currentDay)).toBe(31);
+		});
+
+		it("Solution", () => {
+			expect(solveB("input", currentDay)).toBe(29379307);
 		});
 	});
 });
--- 01/01.ts
+++ 01/01.ts
@@ -4,14 +4,43 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const lists = processList(data);
+	return calculateTotalDistance(lists);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const lists = processList(data);
+	return calculateSimialryScore(lists);
 }
 
-//Run
-solveA("example_a", "01");
-
 // Functions
+function processList(list: string): number[][] {
+	const pairs: string[] = list.split("\n");
+	const listA: number[] = [];
+	const listB: number[] = [];
+
+	for (const pair of pairs) {
+		const [a, b] = pair.split(/\s+/).map(Number);
+		listA.push(a);
+		listB.push(b);
+	}
+
+	return [listA, listB].map((list) => list.sort((a, b) => a - b));
+}
+
+function calculateTotalDistance(lists: number[][]): number {
+	return lists[0].reduce((acc, cur, i) => acc + Math.abs(cur - lists[1][i]), 0);
+}
+
+function calculateSimialryScore(lists: number[][]): number {
+	const frequencyMap = new Map<number, number>();
+
+	for (const n of lists[1]) {
+		frequencyMap.set(n, (frequencyMap.get(n) || 0) + 1);
+	}
+
+	return lists[0].reduce(
+		(acc, cur) => acc + cur * (frequencyMap.get(cur) ?? 0),
+		0
+	);
+}
