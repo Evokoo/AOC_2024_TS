e05380506bc97fa448ced806274380054d7f3044
Evokoo <evanjohncooper@gmail.com>
2024-12-02 19:15:15 +1100

    Day 03 created

---
+++ 03/03.ts
@@ -0,0 +1,17 @@
+// Imports
+import TOOLS from "tools";
+
+//Solutions
+export function solveA(fileName: string, day: string): number {
+	const data = TOOLS.readData(fileName, day);
+	return 0;
+}
+export function solveB(fileName: string, day: string): number {
+	const data = TOOLS.readData(fileName, day);
+	return 0;
+}
+
+//Run
+solveA("example_a", "03");
+
+// Functions
