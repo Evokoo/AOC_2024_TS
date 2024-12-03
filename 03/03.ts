60c84f19f5e429286b738df763ece1256d35ebbe
Evokoo <evanjohncooper@gmail.com>
2024-12-03 16:29:06 +1100

    Day 03 complete

--- 03/03.ts
+++ 03/03.ts
@@ -4,14 +4,47 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return conditionalMuliplcationSum(
+		data,
+		RegExp("mul\\(\\d{1,3},\\d{1,3}\\)", "g")
+	);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return conditionalMuliplcationSum(
+		data,
+		RegExp("mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don\\'t\\(\\)", "g")
+	);
 }
 
-//Run
-solveA("example_a", "03");
-
 // Functions
+function getProduct(equation: string): number {
+	return (equation.match(/\d+/g) || []).reduce(
+		(acc, cur) => acc * Number(cur),
+		1
+	);
+}
+
+function conditionalMuliplcationSum(
+	data: string,
+	re: RegExp,
+	sum: number = 0
+): number {
+	const instructions = data.match(re) || [];
+
+	for (let i = 0, enabled = true; i < instructions.length; i++) {
+		const instruction = instructions[i];
+
+		if (instruction.startsWith("mul")) {
+			sum += enabled ? getProduct(instruction) : 0;
+		} else if (instruction.startsWith("don't")) {
+			enabled = false;
+		} else if (instruction.startsWith("do")) {
+			enabled = true;
+		} else {
+			throw Error(`Invalid Instruciton: ${instruction}`);
+		}
+	}
+
+	return sum;
+}
