6364c454753b5d2b4e4b588f3be6f719e00e529c
Evokoo <evanjohncooper@gmail.com>
2024-12-11 19:10:56 +1100

    Day 12 created

---
+++ 12/12.test.ts
@@ -0,0 +1,28 @@
+import * as path from "@std/path";
+import { expect } from "@std/expect";
+import { describe, it } from "@std/testing/bdd";
+import { solveA, solveB } from "./12.ts";
+
+const currentDay = path.basename(Deno.cwd());
+
+describe(`AOC 2024 - Day ${currentDay}`, () => {
+	describe("Part A", () => {
+		it("Example", () => {
+			expect(solveA("example_a", currentDay)).toBe(0);
+		});
+
+		it.ignore("Solution", () => {
+			expect(solveA("input", currentDay)).toBe(0);
+		});
+	});
+
+	describe.ignore("Part B", () => {
+		it("Example", () => {
+			expect(solveB("example_b", currentDay)).toBe(0);
+		});
+
+		it.ignore("Solution", () => {
+			expect(solveB("input", currentDay)).toBe(0);
+		});
+	});
+});
---
+++ 12/12.ts
@@ -0,0 +1,14 @@
+// Imports
+import TOOLS from "tools";
+
+//Solutions
+export function solveA(fileName: string, day: string): number {
+	const data = TOOLS.readData(fileName, day);
+	return 0;
+}
+export function solveB(fileName: string, day: string): number {
+	const data = TOOLS.readData(fileName, day);
+	return 0;
+}
+
+// Functions
