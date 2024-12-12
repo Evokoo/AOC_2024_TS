e9226e0fe2bfcfc3629e9ab27ed7adcd23836025
Evokoo <evanjohncooper@gmail.com>
2024-12-12 22:08:57 +1100

    Day 12 complete

--- 12/12.ts
+++ 12/12.ts
@@ -5,17 +5,16 @@ import TOOLS from "tools";
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
 	const grid: Grid = parseInput(data);
-	const plots: Farm = findPlots(grid);
-	return calculateCost(plots);
+	return pricePlots(grid);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const grid: Grid = parseInput(data);
+	return pricePlots(grid, true);
 }
 
 type Grid = string[][];
 type Point = { x: number; y: number };
-type Farm = Map<string, number>;
 
 interface Plot {
 	id: string;
@@ -28,10 +27,9 @@ interface Plot {
 function parseInput(data: string): Grid {
 	return data.split("\n").map((row) => [...row]);
 }
-
-function findPlots(grid: Grid): Farm {
+function pricePlots(grid: Grid, sides: boolean = false): number {
 	const seen: Set<string> = new Set();
-	const plotData: Map<string, number> = new Map();
+	let plotCost: number = 0;
 
 	for (let y = 0; y < grid.length; y++) {
 		for (let x = 0; x < grid[0].length; x++) {
@@ -41,7 +39,11 @@ function findPlots(grid: Grid): Farm {
 				const id = grid[y][x];
 				const plot = BFS(id, x, y, grid);
 
-				plotData.set(id, (plotData.get(id) ?? 0) + plot.area * plot.perimeter);
+				if (sides) {
+					plotCost += plot.area * sideCount(plot);
+				} else {
+					plotCost += plot.area * plot.perimeter;
+				}
 
 				for (const coord of plot.coords) {
 					seen.add(coord);
@@ -50,7 +52,7 @@ function findPlots(grid: Grid): Farm {
 		}
 	}
 
-	return plotData;
+	return plotCost;
 }
 function BFS(id: string, x: number, y: number, grid: Grid): Plot {
 	const plot: Plot = { id, perimeter: 0, area: 0, coords: new Set() };
@@ -84,12 +86,73 @@ function BFS(id: string, x: number, y: number, grid: Grid): Plot {
 	}
 	return plot;
 }
-function calculateCost(farm: Farm): number {
-	let totalCost = 0;
+function sideCount(plot: Plot): number {
+	const max: Point = { x: -Infinity, y: -Infinity };
+	const min: Point = { x: Infinity, y: Infinity };
 
-	for (const [_, price] of farm) {
-		totalCost += price;
+	[...plot.coords].forEach((coord) => {
+		const [x, y] = coord.split(",").map(Number);
+		max.x = Math.max(x, max.x);
+		max.y = Math.max(y, max.y);
+		min.x = Math.min(x, min.x);
+		min.y = Math.min(y, min.y);
+	});
+
+	let sides = 0;
+
+	//Horiztonal
+	for (let y = min.y; y <= max.y; y++) {
+		for (let x = min.x; x <= max.x; x++) {
+			const coord = `${x},${y}`;
+
+			if (!plot.coords.has(coord)) {
+				continue;
+			} else {
+				const isShape: Record<string, boolean> = {
+					N: false,
+					S: false,
+					E: false,
+					W: false,
+					NE: false,
+					NW: false,
+					SE: false,
+					SW: false,
+				};
+
+				for (const { direction, move } of [
+					{ direction: "N", move: { x: 0, y: -1 } },
+					{ direction: "S", move: { x: 0, y: 1 } },
+					{ direction: "E", move: { x: 1, y: 0 } },
+					{ direction: "W", move: { x: -1, y: 0 } },
+					{ direction: "NE", move: { x: 1, y: -1 } },
+					{ direction: "SE", move: { x: 1, y: 1 } },
+					{ direction: "NW", move: { x: -1, y: -1 } },
+					{ direction: "SW", move: { x: -1, y: 1 } },
+				]) {
+					if (plot.coords.has(`${x + move.x},${y + move.y}`)) {
+						isShape[direction] = true;
+					}
+				}
+
+				//Top Right Corner
+				if (!isShape.N && !isShape.W) sides++;
+				//Top Left Corner
+				if (!isShape.N && !isShape.E) sides++;
+				//Bottom Right Corner
+				if (!isShape.S && !isShape.W) sides++;
+				//Bottom Left Corner
+				if (!isShape.S && !isShape.E) sides++;
+
+				//Inner Top Left
+				if (isShape.E && isShape.S && !isShape.SE) sides++;
+				//Inner Top Right
+				if (isShape.W && isShape.S && !isShape.SW) sides++;
+				//Inner Bottom Left
+				if (isShape.N && isShape.E && !isShape.NE) sides++;
+				//Inner Bottom Right
+				if (isShape.N && isShape.W && !isShape.NW) sides++;
+			}
+		}
 	}
-
-	return totalCost;
+	return sides;
 }
