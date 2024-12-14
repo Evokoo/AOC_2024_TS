315068db1c1c68c8e6b2b3fcce1cf04e385d1806
Evokoo <evanjohncooper@gmail.com>
2024-12-14 16:41:30 +1100

    Day 14 - Part 1 complete

--- 14/14.test.ts
+++ 14/14.test.ts
@@ -8,21 +8,21 @@ const currentDay = path.basename(Deno.cwd());
 describe(`AOC 2024 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay, { x: 11, y: 7 })).toBe(12);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay, { x: 101, y: 103 })).toBe(218295000);
 		});
 	});
 
-	describe.skip("Part B", () => {
-		it("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(0);
+	describe("Part B", () => {
+		it.skip("Example", () => {
+			expect(solveB("example_b", currentDay, { x: 11, y: 7 })).toBe(0);
 		});
 
 		it.skip("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+			expect(solveB("input", currentDay, { x: 101, y: 103 })).toBe(0);
 		});
 	});
 });
--- 14/14.ts
+++ 14/14.ts
@@ -2,13 +2,101 @@
 import TOOLS from "tools";
 
 //Solutions
-export function solveA(fileName: string, day: string): number {
+export function solveA(fileName: string, day: string, gridSize: Point): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const robots: Robot[] = parseInput(data);
+	const safetyFactor: number = simulateRobots(robots, 101, gridSize);
+	return safetyFactor;
 }
-export function solveB(fileName: string, day: string): number {
+export function solveB(fileName: string, day: string, gridSize: Point): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const robots: Robot[] = parseInput(data);
+	simulateRobots(robots, 101, gridSize);
+	return -1;
+}
+
+type Point = { x: number; y: number };
+
+interface Robot {
+	id: number;
+	pos: Point;
+	vel: Point;
 }
 
 // Functions
+function parseInput(data: string): Robot[] {
+	const robots: Robot[] = data.split("\n").map((line, index) => {
+		const [px, py, vx, vy] = (line.match(/-*\d+/g) || []).map(Number);
+		return { id: index, pos: { x: px, y: py }, vel: { x: vx, y: vy } };
+	});
+
+	return robots;
+}
+
+function simulateRobots(
+	robots: Robot[],
+	time: number,
+	gridSize: Point
+): number {
+	for (let second = 0; second < time; second++) {
+		robots = robots.map((robot) => updateRobot(robot, gridSize));
+
+		if (time !== 100) {
+			printFormation(robots, gridSize, second);
+		}
+	}
+	return getSafetyFactor(robots, gridSize);
+}
+function updateRobot({ id, pos, vel }: Robot, gridSize: Point): Robot {
+	pos.x += vel.x;
+	pos.y += vel.y;
+
+	if (pos.x >= gridSize.x) {
+		pos.x -= gridSize.x;
+	} else if (pos.x < 0) {
+		pos.x += gridSize.x;
+	}
+
+	if (pos.y >= gridSize.y) {
+		pos.y -= gridSize.y;
+	} else if (pos.y < 0) {
+		pos.y += gridSize.y;
+	}
+
+	return { id, pos, vel };
+}
+function getSafetyFactor(robots: Robot[], gridSize: Point): number {
+	const quadrants: number[] = [0, 0, 0, 0];
+	const verticalCenter = ~~(gridSize.y / 2);
+	const horiztonalCenter = ~~(gridSize.x / 2);
+
+	for (const { pos } of robots) {
+		if (pos.x < horiztonalCenter) {
+			if (pos.y < verticalCenter) quadrants[0]++;
+			if (pos.y > verticalCenter) quadrants[1]++;
+		} else if (pos.x > horiztonalCenter) {
+			if (pos.y < verticalCenter) quadrants[2]++;
+			if (pos.y > verticalCenter) quadrants[3]++;
+		}
+	}
+
+	return quadrants.reduce((acc, cur) => acc * cur, 1);
+}
+function printFormation(
+	robots: Robot[],
+	gridSize: Point,
+	second: number
+): void {
+	const grid: string[][] = Array.from({ length: gridSize.y }, () =>
+		Array.from({ length: gridSize.x }, () => ".")
+	);
+
+	for (const { pos } of robots) {
+		grid[pos.y][pos.x] = "*";
+	}
+
+	const image: string = grid.map((row) => row.join("")).join("\n");
+
+	console.log({ second, image });
+	debugger;
+}
