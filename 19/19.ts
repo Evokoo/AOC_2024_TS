49a226066a89d3bc23c20661075a1a2e5a4b9f7e
Evokoo <evanjohncooper@gmail.com>
2024-12-19 19:10:58 +1100

    Day 19 complete

--- 19/19.ts
+++ 19/19.ts
@@ -10,85 +10,50 @@ export function solveA(fileName: string, day: string): number {
 
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const { patterns, designs }: Towels = parseInput(data);
+	return validateDesigns({ patterns, designs }, true);
 }
-
-type Patterns = {
-	available: Set<string>;
-	maxLength: number;
-};
 
 interface Towels {
-	patterns: Patterns;
+	patterns: string[];
 	designs: string[];
 }
-
-type State = {
-	original: string;
-	current: string;
-};
 
 // Functions
 function parseInput(data: string): Towels {
 	const sections = data.split("\n\n");
-	const patterns: Patterns = {
-		available: new Set(),
-		maxLength: 0,
+	return {
+		patterns: sections[0].match(/\w+/g) || [],
+		designs: sections[1].split("\n"),
 	};
-
-	for (const pattern of sections[0].match(/\w+/g) || []) {
-		if (pattern.length > patterns.maxLength) {
-			patterns.maxLength = pattern.length;
-		}
-		patterns.available.add(pattern);
-	}
-
-	return { patterns, designs: sections[1].split("\n") };
 }
 
-function validateDesigns({ patterns, designs }: Towels): number {
-	const queue: State[] = designs.map((design) => ({
-		original: design,
-		current: design,
-	}));
-	const availablePatterns: Set<string> = patterns.available;
-	const sliceSize: number = patterns.maxLength;
-	const validDesigns: Set<string> = new Set();
-	const seen: Map<string, Set<string>> = new Map();
+function validateDesigns(
+	{ patterns, designs }: Towels,
+	combinations: boolean = false
+): number {
+	const memo: Map<string, number> = new Map([["", 1]]);
 
-	while (queue.length) {
-		const state: State = queue.shift()!;
-
-		if (validDesigns.has(state.original)) {
-			continue;
+	function DFS(target: string, count: number = 0): number {
+		if (memo.has(target)) {
+			return memo.get(target)!;
 		}
 
-		if (seen.has(state.original)) {
-			if (seen.get(state.original)!.has(state.current)) {
-				continue;
-			} else {
-				seen.set(
-					state.original,
-					new Set([...seen.get(state.original)!, state.current])
-				);
-			}
-		} else {
-			seen.set(state.original, new Set([state.current]));
-		}
+		for (const pattern of patterns) {
+			const len: number = pattern.length;
 
-		if (state.current === "") {
-			validDesigns.add(state.original);
-			continue;
-		}
-
-		for (let i = Math.min(sliceSize, state.current.length); i > 0; i--) {
-			const slice = state.current.slice(0, i);
-
-			if (availablePatterns.has(slice)) {
-				queue.push({ ...state, current: state.current.slice(i) });
+			if (target.slice(0, len) === pattern) {
+				count += DFS(target.slice(len));
 			}
 		}
+
+		memo.set(target, count);
+		return count;
 	}
 
-	return validDesigns.size;
+	if (combinations) {
+		return designs.reduce((acc, cur) => acc + DFS(cur), 0);
+	} else {
+		return designs.map((design) => DFS(design)).filter(Boolean).length;
+	}
 }
