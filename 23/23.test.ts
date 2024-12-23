52569eda66d036cc8f68200dd43c12d9578a2141
Evokoo <evanjohncooper@gmail.com>
2024-12-23 17:48:23 +1100

    Day 23 complete

--- 23/23.test.ts
+++ 23/23.test.ts
@@ -8,21 +8,23 @@ const currentDay = path.basename(Deno.cwd());
 describe(`AOC 2024 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		it("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(0);
+			expect(solveA("example_a", currentDay)).toBe(7);
 		});
 
-		it.skip("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveA("input", currentDay)).toBe(1119);
 		});
 	});
 
-	describe.skip("Part B", () => {
+	describe("Part B", () => {
 		it("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(0);
+			expect(solveB("example_b", currentDay)).toBe("co,de,ka,ta");
 		});
 
-		it.skip("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(0);
+		it("Solution", () => {
+			expect(solveB("input", currentDay)).toBe(
+				"av,fr,gj,hk,ii,je,jo,lq,ny,qd,uq,wq,xc"
+			);
 		});
 	});
 });
--- 23/23.ts
+++ 23/23.ts
@@ -4,11 +4,99 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const computers: ComputerMap = parseInput(data);
+	return networkSets(computers);
 }
-export function solveB(fileName: string, day: string): number {
+export function solveB(fileName: string, day: string): string {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const computers: ComputerMap = parseInput(data);
+	return findPassword(computers);
 }
 
+type ComputerMap = Map<string, Set<string>>;
+type State = { computer: string; connections: string[] };
+type Result = { arr: string[]; length: number };
+
 // Functions
+function parseInput(data: string): ComputerMap {
+	const network: ComputerMap = new Map();
+
+	for (const line of data.split("\n")) {
+		const [a, b] = line.split("-");
+
+		network.set(a, new Set([...(network.get(a) ?? []), b]));
+		network.set(b, new Set([...(network.get(b) ?? []), a]));
+	}
+
+	return network;
+}
+function networkSets(computers: ComputerMap): number {
+	const groups: Set<string> = new Set();
+
+	for (const [com1, connectedTo] of computers) {
+		const connections = [...connectedTo];
+
+		for (let i = 0; i < connections.length; i++) {
+			const com2: string = connections[i];
+
+			for (let j = i + 1; j < connections.length; j++) {
+				const com3: string = connections[j];
+
+				if (computers.get(com2)!.has(com3)) {
+					const sequence = [com1, com2, com3].sort().join(",");
+
+					if (/t\w/.test(sequence)) {
+						groups.add(sequence);
+					}
+				}
+			}
+		}
+	}
+
+	return groups.size;
+}
+function findPassword(computers: ComputerMap): string {
+	const output: Result = { arr: [], length: 0 };
+
+	function DFS(computer: string): void {
+		const queue: State[] = [{ computer, connections: [computer] }];
+		const result: Result = { arr: [], length: 0 };
+
+		while (queue.length) {
+			const { computer, connections }: State = queue.pop()!;
+
+			if (connections.length < result.length) {
+				continue;
+			}
+
+			for (const testComputer of computers.get(computer)!) {
+				const testConnections = computers.get(testComputer)!;
+
+				if (connections.every((com) => testConnections.has(com))) {
+					queue.push({
+						computer: testComputer,
+						connections: [...connections, testComputer],
+					});
+				} else {
+					result.arr = connections;
+					result.length = connections.length;
+				}
+			}
+		}
+
+		if (result.length > output.length) {
+			output.arr = result.arr;
+			output.length = result.length;
+		}
+	}
+
+	for (const [computer, _] of computers) {
+		if (output.arr.includes(computer)) {
+			continue;
+		} else {
+			DFS(computer);
+		}
+	}
+
+	return output.arr.sort().join(",");
+}
