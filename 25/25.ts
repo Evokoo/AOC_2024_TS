0d85dd3432edf22f882c3b7c13019b5d19cc7520
Evokoo <evanjohncooper@gmail.com>
2024-12-25 18:44:50 +1100

    Day 25 - Part 1 complete

--- 25/25.ts
+++ 25/25.ts
@@ -4,7 +4,7 @@ import TOOLS from "tools";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return parseInput(data);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
@@ -12,3 +12,36 @@ export function solveB(fileName: string, day: string): number {
 }
 
 // Functions
+function parseInput(data: string): number {
+	const schematics: string[] = data.split("\n\n");
+	const keys: number[][] = [];
+	const locks: number[][] = [];
+
+	for (const schematic of schematics) {
+		const grid: string[][] = schematic.split("\n").map((row) => [...row]);
+		const isLock: boolean = grid[0].every((cell) => cell === "#");
+		const design: number[] = Array.from({ length: grid[0].length }, () => 0);
+
+		for (let col = 0; col < grid[0].length; col++) {
+			for (let row = 0; row < grid.length; row++) {
+				if (grid[row][col] === "#") {
+					design[col]++;
+				}
+			}
+		}
+
+		isLock ? locks.push(design) : keys.push(design);
+	}
+
+	let matches = 0;
+
+	for (const key of keys) {
+		for (const lock of locks) {
+			if (key.every((pin, index) => pin + lock[index] <= 7)) {
+				matches++;
+			}
+		}
+	}
+
+	return matches;
+}
